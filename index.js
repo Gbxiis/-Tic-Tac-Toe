564045799e83dfac7218af550865cd18f075a28a
Gabriel Bomfim <gabrielbomfimfelipe@gmail.com>
2023-02-08 17:23:44 -0300

    js concluido ultimas modificacoes

--- index.js
+++ index.js
@@ -1,9 +1,96 @@
- document.getElementById("0").innerHTML = "0"
- document.getElementById("1").innerHTML = "1"
- document.getElementById("2").innerHTML = "2"
- document.getElementById("3").innerHTML = "3"
- document.getElementById("4").innerHTML = "4"
- document.getElementById("5").innerHTML = "5"
- document.getElementById("6").innerHTML = "6"
- document.getElementById("7").innerHTML = "7"
- document.getElementById("8").innerHTML = "8"
+let board = ["", "", "", "", "", "", "", "", ""];
+
+let playerTime = 0;
+
+let gamerOver = false;
+
+let symbol = ["o", "x"];
+
+const winStates = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const isWin = () => {
+  for (let i = 0; i < winStates.length; i++) {
+    let sequence = winStates[i];
+
+    let position1 = sequence[0];
+    let position2 = sequence[1];
+    let position3 = sequence[2];
+
+    if (
+      board[position1] == board[position2] &&
+      board[position1] == board[position3] &&
+      board[position1] != ""
+    ) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const handleMove = (positon) => {
+  if (gamerOver) {
+    return;
+  }
+
+  if (board[positon] == "") {
+    board[positon] = symbol[playerTime];
+
+    gamerOver = isWin();
+
+    if (!gamerOver) {
+      playerTime = playerTime == 0 ? 1 : 0;
+    }
+  }
+  return gamerOver;
+};
+
+let squares = document.querySelectorAll(".square");
+
+const reset = document.getElementById("clear");
+
+const handleClick = (event) => {
+  let positon = event.target.id;
+
+  if (handleMove(positon)) {
+    setTimeout(() => {
+      alert("THE GAME IS OVER - winner was " + playerTime);
+    }, 30);
+  }
+  updateSquares();
+};
+
+const updateSquares = () => {
+  squares = document.querySelectorAll(".square");
+
+  squares.forEach((square) => {
+    let positon = square.id;
+    let symbol = board[positon];
+
+    if (symbol != "") {
+      square.innerHTML = `<div class='${symbol}'></div>`;
+    }
+    const clear = () => {
+      board = ["", "", "", "", "", "", "", "", ""];
+      playerTime = 0;
+
+      gamerOver = false;
+
+      square.innerHTML = "";
+    };
+
+    reset.addEventListener("click", clear);
+  });
+};
+
+squares.forEach((square) => {
+  square.addEventListener("click", handleClick);
+});
